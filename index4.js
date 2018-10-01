9bcf1b95bf5941e9d658bf31d4ecc6b1239252ae
David <david.khayutin@gmail.com>
2018-10-01 19:52:04 +0000

    next commit - made it module

---
+++ index4.js
@@ -0,0 +1,35 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+function getHTML (options, callback) {
+  var newData = "";
+  https.get(options, function(response){
+
+  response.setEncoding('utf8');
+
+  response.on('data', function(data){
+    newData += data;
+  });
+
+  response.on("end", function(){
+    callback(newData);
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions,printHTML)
+
+module.exports = {
+  getHTML,
+  printHTML
+}
